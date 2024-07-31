590894c0c7171c2f2b41aa83734f328451910be9
PIRESAAO <aaopires@gmail.com>
2024-07-31 20:15:26 -0300

    Integrando Google Maps para captura de coordenadas

--- geofence_authenticator.py
+++ geofence_authenticator.py
@@ -2,48 +2,87 @@ import streamlit as st
 
 st.title("Autenticação Baseada em Geolocalização")
 
-# Função JavaScript para obter coordenadas do usuário
+# Inserindo o código HTML e JavaScript no Streamlit
 st.markdown(
     """
-    <script>
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition, showError);
-        } else {
-            document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser.";
+    <style>
+        .gfg {
+            font-size: 40px;
+            font-weight: bold;
+            color: #009900;
+            margin-left: 20px;
         }
-    }
 
-    function showPosition(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        document.getElementById("latitude").innerHTML = lat;
-        document.getElementById("longitude").innerHTML = lon;
-    }
-
-    function showError(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                document.getElementById("demo").innerHTML = "User denied the request for Geolocation.";
-                break;
-            case error.POSITION_UNAVAILABLE:
-                document.getElementById("demo").innerHTML = "Location information is unavailable.";
-                break;
-            case error.TIMEOUT:
-                document.getElementById("demo").innerHTML = "The request to get user location timed out.";
-                break;
-            case error.UNKNOWN_ERROR:
-                document.getElementById("demo").innerHTML = "An unknown error occurred.";
-                break;
+        .geeks {
+            margin-left: 150px;
         }
-    }
 
+        p {
+            font-size: 20px;
+            margin-left: 20px;
+        }
+    </style>
+
+    <div class="gfg">GeeksforGeeks</div>
+    <p>Display location in map</p>
+    <button class="geeks" type="button" onclick="getlocation();">
+        Current Position
+    </button>
+    <div id="demo2" style="width: 500px; height: 500px"></div>
+
+    <script src="https://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript">
+        function getlocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showLoc, errHand);
+            }
+        }
+        function showLoc(pos) {
+            latt = pos.coords.latitude;
+            long = pos.coords.longitude;
+            var lattlong = new google.maps.LatLng(latt, long);
+            var OPTions = {
+                center: lattlong,
+                zoom: 10,
+                mapTypeControl: true,
+                navigationControlOptions: {
+                    style: google.maps.NavigationControlStyle.SMALL,
+                },
+            };
+            var mapg = new google.maps.Map(
+                document.getElementById("demo2"),
+                OPTions
+            );
+            var markerg = new google.maps.Marker({
+                position: lattlong,
+                map: mapg,
+                title: "You are here!",
+            });
+        }
+
+        function errHand(err) {
+            switch (err.code) {
+                case err.PERMISSION_DENIED:
+                    result.innerHTML =
+                        "The application doesn't have the permission" +
+                        "to make use of location services";
+                    break;
+                case err.POSITION_UNAVAILABLE:
+                    result.innerHTML = 
+                        "The location of the device is uncertain";
+                    break;
+                case err.TIMEOUT:
+                    result.innerHTML = 
+                        "The request to get user location timed out";
+                    break;
+                case err.UNKNOWN_ERROR:
+                    result.innerHTML =
+                        "Time to fetch location information exceeded" +
+                        "the maximum timeout interval";
+                    break;
+            }
+        }
     </script>
-
-    <button onclick="getLocation()">Obter Coordenadas</button>
-    <p>Latitude: <span id="latitude"></span></p>
-    <p>Longitude: <span id="longitude"></span></p>
-    <p id="demo"></p>
     """,
     unsafe_allow_html=True,
 )
