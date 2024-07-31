8044e117aec48d5fa68aec83ff286122dbfce934
PIRESAAO <aaopires@gmail.com>
2024-07-31 20:32:32 -0300

    Captura de coordenadas com streamlit_js_eval com label

--- geofence_authenticator.py
+++ geofence_authenticator.py
@@ -22,20 +22,22 @@ function showPosition(position) {
 }
 
 function showError(error) {
+    var message;
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            document.getElementById("demo").innerHTML = "User denied the request for Geolocation.";
+            message = "User denied the request for Geolocation.";
             break;
         case error.POSITION_UNAVAILABLE:
-            document.getElementById("demo").innerHTML = "Location information is unavailable.";
+            message = "Location information is unavailable.";
             break;
         case error.TIMEOUT:
-            document.getElementById("demo").innerHTML = "The request to get user location timed out.";
+            message = "The request to get user location timed out.";
             break;
         case error.UNKNOWN_ERROR:
-            document.getElementById("demo").innerHTML = "An unknown error occurred.";
+            message = "An unknown error occurred.";
             break;
     }
+    document.getElementById("demo").innerHTML = message;
     document.dispatchEvent(new CustomEvent("returnCoords", {detail: null}));
 }
 
